0c468591f7156a1a65f9549eef0062ff2533b833
jemesza1 <143461678+jemesza1@users.noreply.github.com>
2026-08-15 04:32:35 +0100

    HOTFIX: load core modules from last good commit on jsDelivr until full restore

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 (function(){
 function load(src){return new Promise(function(res,rej){var s=document.createElement("script");s.src=src;s.onload=res;s.onerror=function(){rej(src)};document.head.appendChild(s);});}
-var files=["a.js","dash-fix.js","b1.js","b2a.js","b2b.js","c1.js","c2.js"];
+var GOOD='https://cdn.jsdelivr.net/gh/jemesza1/facture-pro@88fa9a3e24a2df7049507a7286e66175fa858a5a/';
+var files=[
+  GOOD+'a.js',
+  'dash-fix.js',
+  'b1.js',
+  'b2a.js',
+  'b2b.js',
+  GOOD+'c1.js',
+  GOOD+'c2.js'
+];
 files.reduce(function(p,f){return p.then(function(){return load(f);});},Promise.resolve())
 .then(function(){
   try{if(typeof applyLocale==="function")applyLocale();}catch(e){}
