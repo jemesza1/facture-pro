822498922e90a8f9bf640738a594a1d29205b63c
jemesza1 <143461678+jemesza1@users.noreply.github.com>
2026-08-15 04:32:17 +0100

    HOTFIX: restore a.js c1.js c2.js from last good commit via jsDelivr loaders

--- c2.js
+++ c2.js
@@ -1,1 +1,7 @@
-PLACEHOLDER
+/* HOTFIX loader */
+(function(){
+  var s=document.createElement('script');
+  s.src='https://cdn.jsdelivr.net/gh/jemesza1/facture-pro@88fa9a3e24a2df7049507a7286e66175fa858a5a/c2.js';
+  s.async=false;
+  document.head.appendChild(s);
+})();
